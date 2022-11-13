9032714ca7718a12ffc00536da2d357757facd28
Atharva <adrooney322@gmail.com>
2022-11-13 11:21:26 +0000

    final changes

--- client/src/components/utils/constants.js
+++ client/src/components/utils/constants.js
@@ -1,5 +1,5 @@
-export const source = 'oxford-lfg'
+export const source = 'oxford-lfg1'
 
 export const WATER_CONTRACT_ADDRESS = "0xa3f0229303874b24cac252508BfB5CC19Ce82AD4"
 
-export const SEED_CONTRACT_ADDRESS = "0xA53c3Af634f3bf736FA62d9943720fb93205e42F"
+export const SEED_CONTRACT_ADDRESS = "0x1830d75f3b3519d7e69469cfd1513d2fa3eeb430"
--- client/src/pages/Tree.js
+++ client/src/pages/Tree.js
@@ -65,13 +65,12 @@ const Tree = () => {
     setLevel(parseInt(level.level))
     setTimetoupgrade(parseInt(level.timeToUpgrade))
     if (parseInt(level.level) == 0) {
-      alert("hdhdh")
       setImageshown("Tree1.png")
     } else if (parseInt(level.level) == 1) {
       setImageshown("Tree2.png")
-    }else if (parseInt(level.level) == 2) {
+    } else if (parseInt(level.level) == 2) {
       setImageshown("Tree3.png")
-    }else if (parseInt(level.level) == 3) {
+    } else if (parseInt(level.level) == 3) {
       setImageshown("Tree4.png")
     } else {
       setImageshown("Tree5.png")
@@ -99,12 +98,12 @@ const Tree = () => {
           </Typography>
         </Stack>
       </Container>
-      <Container maxWidth="md">
+      {timetoupgrade > 0 ? <Container maxWidth="md">
         <Stack sx={{ mt: 5, textAlign: "center" }}>
           <Input type="number" placeholder="Enter amount of water" onChange={handleChange} />
           <Button size='small' onClick={handleUpgrade}>Water</Button>
         </Stack>
-      </Container>
+      </Container> : null}
     </Box>
 
   );
