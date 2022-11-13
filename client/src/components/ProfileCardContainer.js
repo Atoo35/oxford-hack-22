7b19af43be2063bb22ba43c161c5461fb7c68e7d
Undefeated0725 <zhaojingbo0104@163.com>
2022-11-13 09:17:41 +0000

    ch

--- client/src/components/ProfileCardContainer.js
+++ client/src/components/ProfileCardContainer.js
@@ -2,16 +2,8 @@ import { Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import ProfileCard from "./ProfileCard";
 import { getUserNFTs } from "./utils/LensProtocol/publication";
-import { source } from './utils/constants'
 import { getSigner } from "./utils/common";
 const ProfileCardContainer = () => {
-
-  // const request = {
-  //   ownerAddress: accounts[0],
-  //   chainIds: [80001],
-  // }
-  // const result = await getUserNFTs(request)
-  // console.log(result)
 
   const getNFTS= async()=>{
     const signer = getSigner();
@@ -29,22 +21,6 @@ const ProfileCardContainer = () => {
   }, [])
 
   const [cardData, setCardData] = useState([]);
-
-//   const getPublications = async () => {
-//     const request = {
-//       sortCriteria: "LATEST",
-//       publicationTypes: ["POST"],
-//       sources: [source],
-//     };
-//     const publications = await explorePublications(request);
-//     setCardData(publications.data.explorePublications.items);
-//   };
-
-
-
-//   useEffect(() => {
-//     getPublications();
-//   }, []);
 
   return (
     <Grid container sx={{ mt: 3 }}>
